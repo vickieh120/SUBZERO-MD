cd1a60fa2a025e2b1da355f62674d733c5d9dfb3
Thuita <86429884+vickieh120@users.noreply.github.com>
2025-07-21 01:30:21 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;WENQ2DYb#fdscqVakzxgC3ehZCW0Ta1_-LfVzYCbLhlGf4bC0KyA", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;PBMRRSJa#KjPVp8ApFLAxuC_5JhMXmNnjVAPre9YzzOAK1cSvn2c", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
