77b9f0e0eed6f54ed7b7f9fc3e37520fae6626f9
Thuita <86429884+vickieh120@users.noreply.github.com>
2025-07-25 19:50:34 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;nUVUVY5b#yLWUALp4CmVc4FQASRAyfOeuuEzFYBKDZL_IwzdH9pY", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;HR9lQBoA#CX8bn7onPvpNSqQivvCQQEi_oN7YeJneo0BR_06L_xg", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "254742219531", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
